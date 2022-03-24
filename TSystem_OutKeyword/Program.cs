beaf6b066667263285e8d8c5092393aed41b53b4
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:33:51 +0530

    Add project files.

---
+++ TSystem_OutKeyword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_OutKeyword
+{
+    class Program
+    {
+       
+          static void Calculation(int n1,int n2, out int add,out int sub,out int multiply)
+            {
+            add = n1 + n2;
+            sub = n1 - n2;
+            multiply = n1 * n2;
+
+           
+            }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 30;
+            int add, sub, multiply;
+           Calculation(a,b,out add,out sub,out multiply);
+            Console.WriteLine(add);
+            Console.WriteLine(sub);
+            Console.WriteLine(multiply);
+            Console.ReadLine();
+        }
+       
+        }
+    }
+
